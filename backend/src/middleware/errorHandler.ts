efab0c75f323ffe13da622670209779e0d0b39f2
spydersayor <sayor482005a@gmail.com>
2025-09-20 21:20:29 +0530

    Fourth Commit

--- backend/src/middleware/errorHandler.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-  console.error("🔥 Error:", err);
-  res.status(500).json({ message: "Internal Server Error" });
-}
